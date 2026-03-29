da1c4f4b0ab803ef3d904affb590917c03104615
JhiNResH <dontforgetbananatiff@gmail.com>
2026-03-29 16:25:51 -0700

    feat: add DOM/Big O demos and TodoList V5 sort feature (m5-w4-d2)

---
+++ week4/m5-w4-d2-homework/todo-list-v5/src/AllTodos.js
@@ -0,0 +1,44 @@
+export default function AllTodos(props) {
+  const { lists1, lists2 } = props;
+  return (
+    <div>
+      <h2>Todo Lists</h2>
+      <div className="border mx-5 my-3 py-3">
+        <h3 className="text-info">Monday</h3>
+        {lists1.map((list1) => {
+          return <div key={list1.id}>{list1.text}</div>;
+        })}
+        <button
+          className="mx-1 mt-2 bg-info text-white border-0"
+          onClick={() => props.onSort(lists1, "asc")}
+        >
+          Rush
+        </button>
+        <button
+          className="mx-1 mt-2 bg-info text-white border-0"
+          onClick={() => props.onSort(lists1, "desc")}
+        >
+          Relax
+        </button>
+      </div>
+      <div className="border mx-5 my-3 py-3">
+        <h3 className="text-info">Tuesday</h3>
+        {lists2.map((list2) => {
+          return <div key={list2.id}>{list2.text}</div>;
+        })}
+        <button
+          className="mx-1 mt-2 bg-info text-white border-0"
+          onClick={() => props.onSort(lists2, "asc")}
+        >
+          Rush
+        </button>
+        <button
+          className="mx-1 mt-2 bg-info text-white border-0"
+          onClick={() => props.onSort(lists2, "desc")}
+        >
+          Relax
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ week4/m5-w4-d2-homework/todo-list-v5/src/TodoData.js
@@ -0,0 +1,37 @@
+export const TodoData = {
+  TodoList1: [
+    { id: 1, text: "Eat Breakfast", note: "Make Eggs and Pancakes", done: false },
+    { id: 2, text: "Walk Dog", note: "Bring doggie bag", done: false },
+    { id: 3, text: "Take Shower", note: "Shower no later than 10am!", done: false }
+  ],
+  TodoList2: [
+    { id: 1, text: "Get Lunch", note: "Lunch at Sandbox", done: false },
+    { id: 2, text: "Run a Mile", note: "Stop by mom at end of run", done: false },
+    { id: 3, text: "Take Bath", note: "Prep half hour before. Replenish bath oil.", done: false }
+  ],
+  TodoList3: [
+    { id: 1, text: "Buy Groceries", note: "Don't forget milk and eggs!", done: false },
+    { id: 2, text: "Do Laundry", note: "Separate darks from whites.", done: false },
+    { id: 3, text: "Read a Book", note: "Finish chapter 5 today.", done: false }
+  ],
+  TodoList4: [
+    { id: 1, text: "Go to Gym", note: "Leg day - don't skip!", done: false },
+    { id: 2, text: "Cook Dinner", note: "Try the new pasta recipe.", done: false },
+    { id: 3, text: "Call a Friend", note: "Call Sarah about the weekend.", done: false }
+  ],
+  TodoList5: [
+    { id: 1, text: "Clean the House", note: "Start with the kitchen.", done: false },
+    { id: 2, text: "Watch a Movie", note: "That new sci-fi film is out!", done: false },
+    { id: 3, text: "Go for a Walk", note: "Try the new trail in the park.", done: false }
+  ],
+  TodoList6: [
+    { id: 1, text: "Sleep In", note: "You deserve the rest!", done: false },
+    { id: 2, text: "Visit Family", note: "Bring dessert for everyone.", done: false },
+    { id: 3, text: "Plan Next Week", note: "Review your goals first.", done: false }
+  ],
+  TodoList7: [
+    { id: 1, text: "Meal Prep", note: "Prep for the whole week.", done: false },
+    { id: 2, text: "Do Homework", note: "Complete all assignments!", done: false },
+    { id: 3, text: "Get Good Sleep", note: "Aim for 8 hours tonight.", done: false }
+  ]
+};
